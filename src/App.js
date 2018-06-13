8c914b8e231841231e0d52e00073d3b8ddcb78a8
Raman Hundal <rshundal11@gmail.com>
2018-06-13 16:57:29 -0700

    Set up landing page for react app and moving onto file upload

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+/*import Rank from './components/Rank/Rank';*/
+import Title from './components/Title/Title';
+import Particles from 'react-particles-js';
+import axios from 'axios';
 import './App.css';
 
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+        }
+      },
+    interactivity: {
+      onhover: {
+        modes: {
+          repulse:
+          {
+            line_linked: {
+              opacity: .6
+            }
+          }
+        },
+      onclick: {
+        enable: true,
+        modes: {
+          push: {
+            line_linked: {
+              opacity: 1
+            }
+          }
+        }
+       }
+      } 
+    }
+  }
+}
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      selectedFile: null
+    }
+  }
+
+fileSelectedHandler = event => {
+  this.setState({
+    selectedFile: event.target.files[0]
+  })
+}
+
+fileUploadHandler = () => {
+  axios.post('');
+}
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='tc'>
+        <Particles className='particles'
+          params={particlesOptions}
+        />
+        <Title />
+{/*        <Rank /> /*}
+{/*        <ImageForm onInputChange={this.onInputChange} />*/}
+        <input className='pt6 pb5 center f4' type="file" onChange={this.fileSelectedHandler}/>
+        <button className='w-30 grow f4 link ph3 pv2 dib white bg-red center' onClick={this.fileUploadHandler}>Upload</button>
+        {/*<FaceRecognition />*/}
       </div>
     );
   }
